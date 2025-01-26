303cdd7a1893e777ccb4ca6a9b4ced3ad67f646b
rishirokade <rishikeshrokade7@gmail.com>
2025-01-26 21:15:27 +0530

    fixed build issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Make sure this is the correct folder
+  },
   plugins: [react()],
+
 })
